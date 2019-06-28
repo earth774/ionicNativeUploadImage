bb280b9b1664334741ccccfc80cb7c0cda536125
Sutthipong Earth <sutthipongearth@Sutthipongs-MacBook-Pro.local>
2019-06-28 16:18:11 +0700

    first project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { IonicSelectableModule } from 'ionic-selectable';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicSelectableModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Camera,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,99 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, ActionSheetController } from 'ionic-angular';
+import { IonicSelectableComponent } from 'ionic-selectable';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+class Port {
+  public id: number;
+  public name: string;
+}
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  ports: Port[];
+  port: Port;
+  image: string = '';
+
+  constructor(
+    private camera: Camera,
+    public actionSheetCtrl: ActionSheetController
+  ) {
+    this.ports = [
+      { id: 1, name: 'Tokai' },
+      { id: 2, name: 'Vladivostok' },
+      { id: 3, name: 'Navlakhi' }
+    ];
+    this.presentUploadImage().then((data: any) => {
+      this.image = data;
+      console.log(data);
+    });
+  }
+
+  portChange(event: {
+    component: IonicSelectableComponent,
+    value: any
+  }) {
+    console.log('port:', event.value);
+    console.log('port:', this.port.id);
+  }
+
+  getImage(sourceType) {
+    return new Promise((resolve, reject) => {
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        // mediaType: this.camera.MediaType.PICTURE,
+        sourceType: sourceType,
+        correctOrientation: true,
+        saveToPhotoAlbum: false
+      }
+      this.camera.getPicture(options).then((imageData) => {
+        resolve('data:image/jpeg;base64,' + imageData);
+      }, (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  presentUploadImage() {
+    return new Promise((resolve, reject) => {
+      let actionSheet = this.actionSheetCtrl.create({
+        title: 'อัพโหลดรูป',
+        buttons: [
+          {
+            text: 'ถ่ายรูป',
+            icon: 'ios-camera',
+            handler: () => {
+              console.log('Destructive clicked');
+              resolve(this.getImage(1));
+            }
+          },
+          {
+            text: 'เลือกรูป',
+            icon: 'ios-images',
+            handler: () => {
+              resolve(this.getImage(0));
+            }
+          },
+          {
+            text: 'ปิด',
+            icon: 'ios-close',
+            role: 'cancel',
+            handler: () => {
+              resolve('camcel');
+              console.log('Cancel clicked');
+            }
+          }
+        ]
+      });
+
+      actionSheet.present();
+    });
 
   }
+
 
 }
